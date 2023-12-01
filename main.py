e762e12ce1fedbee59814cd1313ee730d6875a30
ewancortes <142259199+ewancortes@users.noreply.github.com>
2023-12-01 12:43:25 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# This file was created by Ewan Cortes
+
+# TITLE = Soldier Runner
+
+# Goals
+'''
+Create a game like the google dinasour game in which the player has to make it
+as far as possible along the track going right
+
+However, change the overall design and add mobs
+
+Mobs will try to attack the player as he moves across the screen
+
+The player will be able to shoot at the mobs but will have to do so before the 
+player gets off screen
+
+The player gets off screen by staying still as the ground moves (left) or player moves right
+''' 

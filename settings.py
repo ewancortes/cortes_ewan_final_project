3bc4ce55145884345ac5b5eaa0b6e35f7e41303a
ewancortes <e.cortes24@bcp.org>
2023-12-07 10:51:09 -0800

    update

--- settings.py
+++ settings.py
@@ -5,26 +5,26 @@ W, H = 800, 437
 win = pg.display.set_mode((W,H))
 pg.display.set_caption('Side Scroller')
 
-bg = pg.image.load(os.path.join('images','bg.png')).convert()
+bg = pg.image.load(os.path.join('images','DinoWallpaper.png')).convert()
 bgX = 0
 bgX2 = bg.get_width()
 
 
 run = [pg.image.load(os.path.join('images', str(x) + '.png')) for x in range(8,16)]
 jump = [pg.image.load(os.path.join('images', str(x) + '.png')) for x in range(1,8)]
-slide = [pg.image.load(os.path.join('images', 'S1.png')),
-         pg.image.load(os.path.join('images', 'S2.png')),
-         pg.image.load(os.path.join('images', 'S2.png')),
-         pg.image.load(os.path.join('images', 'S2.png')), 
-         pg.image.load(os.path.join('images', 'S2.png')),
-         pg.image.load(os.path.join('images', 'S2.png')), 
-         pg.image.load(os.path.join('images', 'S2.png')),
-         pg.image.load(os.path.join('images', 'S2.png')), 
-         pg.image.load(os.path.join('images', 'S3.png')),
-         pg.image.load(os.path.join('images', 'S4.png')), 
-         pg.image.load(os.path.join('images', 'S5.png'))
+slide = [pg.image.load(os.path.join('images', 'DinoDuck1.png')),
+         pg.image.load(os.path.join('images', 'DinoDuck2.png')),
+         pg.image.load(os.path.join('images', 'DinoDuck2.png')),
+         pg.image.load(os.path.join('images', 'DinoDuck2.png')), 
+         pg.image.load(os.path.join('images', 'DinoDuck2.png')),
+         pg.image.load(os.path.join('images', 'DinoDuck2.png')), 
+         pg.image.load(os.path.join('images', 'DinoDuck2.png')),
+         pg.image.load(os.path.join('images', 'DinoDuck2.png')), 
+         pg.image.load(os.path.join('images', 'DinoDead.png')),
+         pg.image.load(os.path.join('images', 'DinoDead.png')), 
+         pg.image.load(os.path.join('images', 'DinoStart.png'))
          ]
-fall = pg.image.load(os.path.join('images','0.png')) # NEW
+fall = pg.image.load(os.path.join('images','DinoDead.png')) # NEW
 
 rotate = [pg.image.load(os.path.join('images', 'LargeCactus1.PNG')),
           pg.image.load(os.path.join('images', 'LargeCactus2.PNG')),
@@ -34,3 +34,5 @@ rotate = [pg.image.load(os.path.join('images', 'LargeCactus1.PNG')),
 
 
 img = pg.image.load(os.path.join('images', 'Bird1.png'))
+
+jumpList = [1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4]
